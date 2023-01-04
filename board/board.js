4da93189b6266ccc2a5343a03d3ebdbb78445e6a
HydraCoding <goekay.sahin@gmx.net>
2023-01-04 17:29:45 +0100

    -set some margins -add fonts -add clickcheck with js

--- board/board.js
+++ board/board.js
@@ -1,1 +1,32 @@
 function init() {}
+
+function popup() {
+  let background = document.getElementById("popup");
+  let card = document.getElementById("popup_card");
+
+  background.classList.toggle("d-none");
+  card.classList.toggle("d-none");
+}
+
+var mouseDown = 0;
+function clicked() {
+  document.getElementById("card").onmousedown = function () {
+    ++mouseDown;
+    console.log(mouseDown);
+    checkClick();
+  };
+}
+document.onmouseup = function () {
+  mouseDown = 0;
+  console.log(mouseDown);
+};
+
+function checkClick() {
+  setTimeout(checkTime, 1200);
+}
+
+function checkTime() {
+  if (mouseDown == 1) {
+    console.log("popup");
+  }
+}
