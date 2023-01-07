dd7a6c0c7dde09f6f6e7a43d136c4e07713aa45f
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-07 15:01:49 +0100

    task-urgent hover

--- summary/summary.js
+++ summary/summary.js
@@ -1,2 +1,12 @@
 var today = new Date();
   document.write(today.toDateString());
+
+  let borderContainer = document.getElementsByClassName('task-urgent');
+  let borderLine = document.getElementsByClassName('border-line');
+  
+  if (borderLine.style.backgroundColor === 'white') {
+    borderLine.style.borderLeft = '3px solid #4589ff';
+  } else {
+    borderLine.style.borderLeft = '3px solid white';
+  }
+  
