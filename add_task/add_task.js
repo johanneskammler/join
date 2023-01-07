a8c2520e62c0647fe99d95e2e7651300688f13a2
Johannes Kammler <johanneskammler@gmail.com>
2023-01-07 14:39:28 +0100

    add_task importance buttons update

---
+++ add_task/add_task.js
@@ -0,0 +1,41 @@
+function fillImportanceButton1() {
+    document.getElementById('importance-button1').style = 'display: none;';
+    document.getElementById('importance-button1-colored').style = 'display: flex; cursor: pointer;';
+    document.getElementById('importance-button2').style = 'display: flex;';
+    document.getElementById('importance-button2-colored').style = 'display: none;';
+    document.getElementById('importance-button3').style = 'display: flex;';
+    document.getElementById('importance-button3-colored').style = 'display: none;';
+}
+
+function emptyImportanceButton1() {
+    document.getElementById('importance-button1').style = 'display: flex;';
+    document.getElementById('importance-button1-colored').style = 'display: none;';
+}
+
+function fillImportanceButton2() {
+    document.getElementById('importance-button2').style = 'display: none;';
+    document.getElementById('importance-button2-colored').style = 'display: flex; cursor: pointer;';
+    document.getElementById('importance-button1').style = 'display: flex;';
+    document.getElementById('importance-button1-colored').style = 'display: none;';
+    document.getElementById('importance-button3').style = 'display: flex;';
+    document.getElementById('importance-button3-colored').style = 'display: none;';
+}
+
+function emptyImportanceButton2() {
+    document.getElementById('importance-button2').style = 'display: flex;';
+    document.getElementById('importance-button2-colored').style = 'display: none;';
+}
+
+function fillImportanceButton3() {
+    document.getElementById('importance-button3').style = 'display: none;';
+    document.getElementById('importance-button3-colored').style = 'display: flex; cursor: pointer;';
+    document.getElementById('importance-button1').style = 'display: flex;';
+    document.getElementById('importance-button1-colored').style = 'display: none;';
+    document.getElementById('importance-button2').style = 'display: flex;';
+    document.getElementById('importance-button2-colored').style = 'display: none;';
+}
+
+function emptyImportanceButton3() {
+    document.getElementById('importance-button3').style = 'display: flex;';
+    document.getElementById('importance-button3-colored').style = 'display: none;';
+}
