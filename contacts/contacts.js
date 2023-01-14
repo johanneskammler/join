192883e90415b96b5352003516ac0ceb172b8bd4
BestOfSpeck <tom.kuestermann76@gmail.com>
2023-01-14 11:02:52 +0100

    contacts update

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,32 +1,9 @@
 async function init() {
     await includeHTML();
     await renderContactList();
-    checkSize();
+
+    checkMainSize();
 }
-
-
-async function renderContactList() {
-    let url = '../contacts.json';
-    let response = await fetch(url);
-    let contacts = await response.json();
-
-    for (let i = 0; i < contacts.length; i++) {
-        const element = contacts[i];
-
-        document.getElementById('content').innerHTML += `
-        <div class="contact">
-                <div class="circle">AM</div>
-                <div class="contact-info-container">
-                     <span class="contact-name">${element['name']}</span>
-                    <span class="contact-email">${element['mail']}</span>
-                </div>
-        </div>
-        `;
-    }
-
-    console.log(contacts)
-}
-
 
 function checkSize() {
     let size = window.innerWidth;
@@ -49,7 +26,29 @@ function sidebarTabled() {
 function enableSidebar() {
     document.getElementById("sidebar").classList.add("sidebar");
     document.getElementById("sidebar").classList.remove("tablet-sidebar");
-    document.getElementById("help-section-btn").classList.remove("d-none");
-    document.getElementById("create-btn-responsive").classList.add("d-none");
-    document.getElementById("header-name-resp").classList.add("d-none");
+    // document.getElementById("help-section-btn").classList.remove("d-none");
+    // document.getElementById("create-btn-responsive").classList.add("d-none");
+    // document.getElementById("header-name-resp").classList.add("d-none");
+}
+
+async function renderContactList() {
+    let url = '../contacts.json';
+    let response = await fetch(url);
+    let contacts = await response.json();
+
+    for (let i = 0; i < contacts.length; i++) {
+        const element = contacts[i];
+
+        document.getElementById('content').innerHTML += `
+        <div class="contact">
+                <div class="circle">AM</div>
+                <div class="contact-info-container">
+                     <span class="contact-name">${element['name']}</span>
+                    <span class="contact-email">${element['mail']}</span>
+                </div>
+        </div>
+        `;
+    }
+
+    console.log(contacts)
 }
