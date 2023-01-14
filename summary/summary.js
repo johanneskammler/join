eb252f8e035d488311895d6f0b47e9e3e36b7775
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-14 19:13:31 +0100

    make summary responsive

--- summary/summary.js
+++ summary/summary.js
@@ -6,11 +6,25 @@ async function init() {
 var today = new Date();
 document.write(today.toDateString());
 
-let borderContainer = document.getElementsByClassName("task-urgent");
+/*let borderContainer = document.getElementsByClassName("task-urgent");
 let borderLine = document.getElementsByClassName("border-line");
 
 if (borderLine.style.backgroundColor === "white") {
   borderLine.style.borderLeft = "3px solid #4589ff";
 } else {
   borderLine.style.borderLeft = "3px solid white";
+}*/
+
+
+let currentTime = new Date().getHours();
+if (currentTime < 12) {
+  document.getElementById('greeting').innerHTML = 'Guten Morgen';
+} else if (currentTime < 18) {
+  document.getElementById('greeting').innerHTML = 'Guten Mittag';
+} else {
+  document.getElementById('greeting').innerHTML = 'Guten Abend';
+}
+
+function goToBoard() {
+  window.location = "../board/board.html";
 }
