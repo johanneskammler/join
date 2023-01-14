fcba46bc65e3cd343e2b139993b8b7c17a93b34c
BestOfSpeck <tom.kuestermann76@gmail.com>
2023-01-14 10:16:01 +0100

    add contact.json to contacts

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,7 +1,14 @@
-function renderContactList() {
+async function init() {
+    await includeHTML();
+    await renderContactList();
+    checkSize();
+}
+
+
+async function renderContactList() {
     let url = '../contacts.json';
-    let response = fetch(url);
-    let contacts = response.json();
+    let response = await fetch(url);
+    let contacts = await response.json();
 
     for (let i = 0; i < contacts.length; i++) {
         const element = contacts[i];
@@ -16,4 +23,33 @@ function renderContactList() {
         </div>
         `;
     }
+
+    console.log(contacts)
+}
+
+
+function checkSize() {
+    let size = window.innerWidth;
+    console.log(size);
+    if (size < 1024) {
+        sidebarTabled();
+    } else if (size > 1024) {
+        enableSidebar();
+    }
+}
+
+function sidebarTabled() {
+    document.getElementById("sidebar").classList.remove("sidebar");
+    document.getElementById("sidebar").classList.add("tablet-sidebar");
+    document.getElementById("help-section-btn").classList.add("d-none");
+    document.getElementById("create-btn-responsive").classList.remove("d-none");
+    document.getElementById("header-name-resp").classList.remove("d-none");
+}
+
+function enableSidebar() {
+    document.getElementById("sidebar").classList.add("sidebar");
+    document.getElementById("sidebar").classList.remove("tablet-sidebar");
+    document.getElementById("help-section-btn").classList.remove("d-none");
+    document.getElementById("create-btn-responsive").classList.add("d-none");
+    document.getElementById("header-name-resp").classList.add("d-none");
 }
