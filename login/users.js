5922adc770a2234c6a7e9511d4bc23e0ee359f4d
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-31 18:11:39 +0100

    make summary responsive

---
+++ login/users.js
@@ -0,0 +1,41 @@
+let users = [];
+let loggedUsers = [];
+let activeUser = [];
+
+
+setURL("https://gruppe-417.developerakademie.net/join/smallest_backend_ever");
+
+async function loadAtStart() {
+    await downloadFromServer();
+    loggedUsers = JSON.parse(await backend.getItem("loggedUser")) || [];
+    users = JSON.parse(await backend.getItem("users")) || [];
+}
+
+
+function loadCurrentUserMail() {
+    let lastUser = loggedUsers.length -1;
+    return loggedUsers[lastUser].email;
+}
+
+
+function loadDataFromCurrentUser() {
+    let currentEmail = loadCurrentUserMail();
+    let loggedInUser = users.filter(a => a.email === currentEmail);
+    return loggedInUser;
+
+}
+
+
+function getCurrentUserName() {
+    let guest = [];
+    guest = loggedUsers.map(a => a.email === "guest@join.com");
+    let lastUser = guest.length -1;
+    if (guest[lastUser] === true) {
+        let name = "Max Kebabmann";
+        return name;
+    } else {
+        let name = loadDataFromCurrentUser();
+        name = name[0].name
+        return name;
+    }
+}
