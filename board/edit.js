ff65cfb947dc07a26f34c1a0ec3c21228c40c4bf
HydraCoding <goekay.sahin@gmx.net>
2023-02-12 16:16:59 +0100

    repair popup

--- board/edit.js
+++ board/edit.js
@@ -27,11 +27,11 @@ async function renderContactsEdit() {
 
 function generateHTMLcontacts(element, i) {
   return `
-    <div class="contacts-list-elem">
+    <div class="contacts-list-elem"  onclick="addContactToTask(${i})">
       <label class="control control-checkbox" id="selected-contact">
         <div class="contacts-list-elem-box">
           <span id="name${i}" class="rendered-contact-name">${element["name"]}</span>
-          <input onclick="addContactToTask(${i})" id="contacts-checkbox-${i}" type="checkbox" value="${element["name"]}" />
+          <input id="contacts-checkbox-${i}" type="checkbox" value="${element["name"]}" />
           <div id="control-indicator${i}" class="control-indicator control-setup"></div>
         </div>
       </label>
@@ -46,6 +46,7 @@ function addContactToTask(i) {
     selectedContacts.push(contact);
     console.log(selectedContacts);
   } else {
+    selectedContacts.slice(`${i}`);
     console.log(selectedContacts);
     return;
   }
