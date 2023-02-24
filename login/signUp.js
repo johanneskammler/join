7d6488224aef8d87381ba72be09e196fe4d35904
BestOfSpeck <tom.kuestermann76@gmail.com>
2023-02-24 17:23:31 +0100

    update contacts

--- login/signUp.js
+++ login/signUp.js
@@ -4,56 +4,66 @@ let emailValue = false;
 setURL("https://gruppe-417.developerakademie.net/join/smallest_backend_ever");
 
 async function signUpUser() {
-  let userName = document.getElementById("user").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("pw").value;
+    let userName = document.getElementById("user").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("pw").value;
 
-  checkIfEmailIsCorrectForm(email);
+    checkIfEmailIsCorrectForm(email);
 
-  if (emailValue == false) {
-    return;
-  }
+    if (emailValue == false) {
+        return;
+    }
 
-  let newUser = {
-    userName: userName,
-    email: email,
-    password: password,
-  };
+    let newUser = {
+        userName: userName,
+        email: email,
+        password: password,
+    };
 
-  storedUsers.push(newUser);
+    storedUsers.push(newUser);
 
-  // await downloadFromServer();
-  await backend.setItem("storedUsers", JSON.stringify(storedUsers));
+    // await downloadFromServer();
+    await backend.setItem("storedUsers", JSON.stringify(storedUsers));
 
-  if (userName == "") {
-    alert("Bitte gib deinen Namen ein.");
-    return false;
-  } else if (email == "") {
-    alert("Bitte gib deine E-Mail-Adresse ein.");
-  } else if (password == "") {
-    alert("Bitte gib ein Passwort ein.");
-  } else {
-    window.location = "../login/startPage.html";
-  }
+    if (userName == "") {
+        alert("Bitte gib deinen Namen ein.");
+        return false;
+    } else if (email == "") {
+        alert("Bitte gib deine E-Mail-Adresse ein.");
+    } else if (password == "") {
+        alert("Bitte gib ein Passwort ein.");
+    } else {
+        newAccCreated();
+        setTimeout(() => {
+            window.location = "../login/startPage.html";
+        }, 1000);
+    }
 
-  emailValue = false;
-  return true;
+    emailValue = false;
+    return true;
+}
+
+function newAccCreated() {
+    document.getElementById('new_acc_created').classList.remove('d-none');
+    setTimeout(() => {
+        document.getElementById('new_acc_created').classList.add('d-none');
+    }, 2500);
 }
 
 function checkIfEmailIsCorrectForm(email) {
-  if (!email.includes('.')) {
-    alert('Incorrect e-mail form!');
-  } else {
-    emailValue = true;
-  }
+    if (!email.includes('.')) {
+        alert('Incorrect e-mail form!');
+    } else {
+        emailValue = true;
+    }
 }
 
 async function retrieveUsers() {
-  await downloadFromServer();
-  let storedJSON = (await backend.getItem("storedUsers")) || [];
-  if (storedJSON.length > 1) {
-    storedUsers = JSON.parse(storedJSON);
-  }
+    await downloadFromServer();
+    let storedJSON = (await backend.getItem("storedUsers")) || [];
+    if (storedJSON.length > 1) {
+        storedUsers = JSON.parse(storedJSON);
+    }
 }
 
 console.log(storedUsers);
