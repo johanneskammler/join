f726dbc3f7842659419c39cdccef7320e0392e55
BestOfSpeck <tom.kuestermann76@gmail.com>
2023-03-06 21:58:20 +0100

    fix php send email

--- login/forgotPassword.js
+++ login/forgotPassword.js
@@ -5,25 +5,6 @@ setURL("https://gruppe-417.developerakademie.net/join/smallest_backend_ever");
 
 let users = [];
 
-
-// async function sendMail() {
-//     try {
-//         const response = await fetch(
-//             "https://gruppe-417.developerakademie.net/join/login/send_mail.php", {
-//                 method: "POST",
-//                 body: fd,
-//             }
-//         );
-
-//         if (response.status === 200) {
-//             this.mailSent = true;
-//             this.contactForm.reset();
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     } finally {}
-// }
-
 async function onSubmit(event) {
     event.preventDefault();
     let formData = new FormData(event.target);
