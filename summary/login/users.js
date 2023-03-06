c739947beeeb57c49bdfa1544789631833264d83
BestOfSpeck <tom.kuestermann76@gmail.com>
2023-03-06 20:47:16 +0100

    delete clone login data

--- summary/login/users.js
+++
@@ -1,7 +0,0 @@
-setURL("https://gruppe-417.developerakademie.net/join/smallest_backend_ever");
-
-async function loadAtStart() {
-  await downloadFromServer();
-  let ShowCurrentUserNameForSummery =
-    JSON.parse(await backend.getItem("currentUser")) || [];
-}
