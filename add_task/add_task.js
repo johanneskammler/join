f9c0fa09d312ef80d9a5fb5c19e7fd0745e27e6b
HydraCoding <goekay.sahin@gmx.net>
2023-03-14 09:27:33 +0100

    board.html setting some css

--- add_task/add_task.js
+++ add_task/add_task.js
@@ -172,8 +172,8 @@ function enableSidebar() {
   document.getElementById("sidebar").classList.add("sidebar");
   document.getElementById("sidebar").classList.remove("tablet-sidebar");
   document.getElementById("help-section-btn").classList.remove("d-none");
-  document.getElementById("create-btn-responsive").classList.add("d-none");
-  document.getElementById("header-name-resp").classList.add("d-none");
+  /*   document.getElementById("create-btn-responsive").classList.add("d-none");
+   */ document.getElementById("header-name-resp").classList.add("d-none");
 }
 
 async function renderContacts() {
